1cb35c5ee7a0af3d4175f2be57c3a7f2ab2d0676
sudevmagar <sudebm789@example.com>
2025-03-07 22:03:03 +0545

    minor changes in the texts of about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,19 +13,19 @@ const About = () => {
     >
       <div className="text-left">
         <h1 className="text-7xl font-bold mb-4">
-          <span className="text-gray-200">Frontend</span>
+          <span className="text-gray-200">Full Stack Developer</span>
           <br />
           <span className="text-gray-400">Developer</span>
         </h1>
         <div className="text-gray-300 max-w-2xl text-lg">
           <TypeAnimation
             sequence={[
-              "Hi! I'm Sudev, an aspiring web developer passionate about creating visually appealing and user-friendly web applications. I love learning new technologies and bringing ideas to life through code.",
-              1000, 
+              "Hi! I'm Sudev, an aspiring full-stack developer passionate about building dynamic and efficient web applications. I enjoy working with both front-end and back-end technologies to create seamless user experiences. Constantly learning and exploring new technologies, I strive to bring ideas to life with clean, scalable, and maintainable code.",
+              1000,
             ]}
             wrapper="p"
-            cursor={false} 
-            speed={60} 
+            cursor={false}
+            speed={80}
           />
         </div>
       </div>
